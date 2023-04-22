58af7c1f567a4bab46cc9300bc38cafd4411b5c9
Ashish Lodhi <ashishslodhi@gmail.com>
2023-04-22 09:23:24 +0000

    bubble sort optimized

--- algorithms/arrays/bubbleSort.js
+++ algorithms/arrays/bubbleSort.js
@@ -1,33 +1,46 @@
-function bubbleSort(arr) {
-  let swapped;
+// 1. Input unsorted elements list
+// 2. Iterate over all the elements of list
+// 3. check adjacent elements, if left is greater than right then swap them.
+// 4. repeat step 2 and step 3 till size of elements of list or till no swap is done.
+// 5. return sorted elements list
 
+function bubbleSort(arr) {
+  let swapped = true;
+  let count = 0;
   do {
     swapped = false;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i + 1] && arr[i] > arr[i + 1]) {
-        let temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
+    console.log("on pass: ", count + 1);
+    for (let i = 0; i < arr.length - 1 - count; i++) {
+      
+      if (arr[i] > arr[i + 1]) {
+        arrayElementsSwap(arr, i, i + 1);
         swapped = true;
       }
     }
+    count++;
   } while (swapped == true);
   return arr;
 }
 
 function bubbleSort1(arr) {
-  let swapped;
+  let swapped = true;
+  // let isSorted = false;
+  // count = 0;
 
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    // For no of passes
     swapped = false;
-    for (let j = 0; j < arr.length; j++) {
+    console.log("on pass: ", i + 1);
+    // isSorted = true;
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      // For comparison in each pass
       if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+        arrayElementsSwap(arr, j, j + 1);
         swapped = true;
+        // isSorted = false;
       }
     }
+    // count++;
     if (swapped == false) {
       break;
     }
@@ -35,8 +48,38 @@ function bubbleSort1(arr) {
   return arr;
 }
 
+function bubbleSort2(arr) {
+  // const n = arr.length;
+  let isSorted = false;
+  for (let i = 0; i < arr.length - 1; i++) {
+    // For no of pass
+    isSorted = true;
+    console.log("on pass: ", i + 1);
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      // For comparison in each pass
+      if (arr[j] > arr[j + 1]) {
+        arrayElementsSwap(arr, j, j + 1);
+        isSorted = false;
+      }
+    }
+    if (isSorted) {
+      return arr;
+    }
+  }
+}
+
+function arrayElementsSwap(array, a, b) {
+  let temp = array[a];
+  array[a] = array[b];
+  array[b] = temp;
+}
+
 const unsortedArr = [4, 3, 7, 1, 5];
+// const unsortedArr = [4, 3, 1, 5, 7];
+
+// const unsortedArr = [1, 3, 5, 7, 9];
 
 console.log("Unsorted array: ", unsortedArr);
-
-console.log("Sorted array: ", bubbleSort1(unsortedArr));
+console.time("execution");
+console.log("Sorted array: ", bubbleSort(unsortedArr));
+console.timeEnd("execution");
